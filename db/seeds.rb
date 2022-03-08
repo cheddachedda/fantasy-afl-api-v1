5d158ecefa36add5cb922071e7027a6c2980d90f
Daniel Nguyen <daniel.n.q.nguyen@gmail.com>
2022-03-09 09:17:54 +1100

    Create main seeds script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+start_time = Time.new
+
+# Runs all seed files in db/seeds/ in alphabetical order of filenames
+Dir[File.join(Rails.root, 'db', 'seed_files', '*.rb')].sort.each { |seed| load seed }
+
+end_time = Time.new
+run_time = (end_time - start_time).round
+
+puts "Seeded in #{ run_time } seconds"
