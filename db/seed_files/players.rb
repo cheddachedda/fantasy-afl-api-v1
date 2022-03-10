aff350b005faf7f6e67cec48e926d1638f7ac7da
Daniel Nguyen <daniel.n.q.nguyen@gmail.com>
2022-03-10 19:56:27 +1100

    Seed players

---
+++ db/seed_files/players.rb
@@ -0,0 +1,18 @@
+require 'csv'
+
+def seed_players
+  CSV.foreach('./db/seed_files/data/players.csv') do |line|    
+    player = {
+      :first_name => line[0],
+      :middle_initial => line[1],
+      :last_name => line[2],
+      :club_id => Club.find_by(abbreviation: line[3]).id,
+      :position => line[4].split('/'),
+      :price => line[5],
+      :dob => DateTime.parse(line[6])
+    }
+
+    Player.create(player)
+    puts "Created player: #{line[0..4].select{ |name| name }.join(' ')}"
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,17 @@
-require_relative './seed_files/clubs.rb'
-require_relative './seed_files/fixtures.rb'
+require_relative './seed_files/clubs'
+require_relative './seed_files/fixtures'
+require_relative './seed_files/players'
 
 Club.destroy_all
 Fixture.destroy_all
+Player.destroy_all
 
 start_time = Time.new
 
 seed_clubs
 seed_fixtures(2021)
 seed_fixtures(2022)
+seed_players
 
 end_time = Time.new
 run_time = (end_time - start_time).round
