41ae7d21a5ff3b18ae967063aca84afa8db4351b
Daniel Nguyen <daniel.n.q.nguyen@gmail.com>
2022-03-11 12:54:10 +1100

    Add 'fantasy_scores' and 'average_fantasy_score' to Player model

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,6 +6,6 @@ class PlayersController < ApplicationController
 
   def show
     player = Player.find(params[:id])
-    render json: player
+    render json: player.to_json
   end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,38 @@
 class Player < ApplicationRecord
+  def club
+    Club.find(club_id)
+  end
+
+  def gamelogs
+    gamelogs = Gamelog.where(player_id: id)
+  end
+
+  def fantasy_scores
+    scores = {}
+
+    gamelogs.each do |game|
+      round = game[:round_no]
+      score = game[:fantasy_score]
+      scores[round] = score
+    end
+
+    scores
+  end
+
+  def average_fantasy_score
+    scores = gamelogs
+              .select{ |game| game[:time_on_ground_percentage] >= 25}
+              .collect(&:fantasy_score)
+    
+    (scores.sum.to_f / scores.size).round(2)
+  end
+
+  def to_json
+    {
+      **attributes,
+      :club => club,
+      :average_fantasy_score => average_fantasy_score,
+      :fantasy_scores => fantasy_scores
+    }
+  end
 end
