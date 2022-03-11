f3751013a41ebdb324513b176d28ac86bfaf7a78
Daniel Nguyen <daniel.n.q.nguyen@gmail.com>
2022-03-11 11:33:32 +1100

    Create basic Player routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :fixtures, only: [:index]
   get '/fixtures/:round' => 'fixtures#round', as: 'round'
   get '/fixtures/:round/:id' => 'fixtures#show', as: 'fixture'
+
+  resources :players, only: [:index, :show]
 end
